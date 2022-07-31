c0fd5563a66eef2dcce074745762511db11499fc
medobarakat <ahmedbarakat2401@gmail.com>
2022-07-31 23:03:34 +0200

    inital

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,40 @@ import {
   Image,
   Dimensions,
   TouchableOpacity,
+  Animated,
 } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 const { width, height } = Dimensions.get("window");
 const Home = () => {
+  const marginTop = useState(new Animated.Value(0))[0];
+  const changeHeight = () => {
+    Animated.timing(marginTop, {
+      toValue: 100,
+      duration: 2000,
+      useNativeDriver: false,
+    }).start();
+  };
+  useEffect(() => {
+    changeHeight();
+  }, []);
   return (
     <View style={styles.container}>
-      <View>
+      <Animated.View
+        style={{
+          marginTop,
+        }}
+      >
         <Image
           source={require("../assets/bg.jpg")}
           style={{
-            height: height / 1.2,
+            height: 500,
             width: width,
           }}
         />
-      </View>
+      </Animated.View>
+      <TouchableOpacity>
+        <Text>click me</Text>
+      </TouchableOpacity>
     </View>
   );
 };
